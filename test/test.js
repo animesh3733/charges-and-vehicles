cfa663f11ba029538993e216cec64b3b0e4faf44
P PADMAPRASAD SHENOY <ppadmaprasadshenoy@gmail.com>
2023-03-28 18:11:43 +0530

    Changes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-const assert = require('assert');
-const { expect } = require('chai');
-const mocha = require('mocha');
+// const assert = require('assert');
+const {expect} = require('chai');
+// const mocha = require('mocha');
 
 const charges = require('../chargers.json');
 const vehicles = require('../vehicles.json');
@@ -12,7 +12,7 @@ describe('Test Suite for Charges and Vehicles JSON files', () => {
       expect(charge.chargerModel).to.be.a('string');
     });
   });
-  
+
   it('should have valid connectors', () => {
     charges.forEach((charge) => {
       expect(charge).to.have.property('connectors');
@@ -22,7 +22,7 @@ describe('Test Suite for Charges and Vehicles JSON files', () => {
       });
     });
   });
-  
+
   it('should have a valid power rating', () => {
     charges.forEach((charge) => {
       expect(charge).to.have.property('powerRating');
@@ -64,7 +64,9 @@ describe('Vehicles JSON', () => {
       expect(vehicle.connectorType).to.have.length.above(0);
       vehicle.connectorType.forEach((connector) => {
         expect(connector).to.be.a('string');
-        expect(connector).to.match(/^(CCS1|CCS2|CHAdeMO|Type 2|Tesla Supercharger|Tesla Wall Connector|GBT)$/);
+        expect(connector).to.match(
+            /^(CCS1|CCS2|CHAdeMO|Type 2|Tesla Supercharger|Tesla Wall Connector|GBT)$/,
+        );
       });
     });
   });
@@ -81,9 +83,10 @@ describe('Vehicles JSON', () => {
     const uniqueModels = new Set(chargerModels);
     expect(uniqueModels.size).to.equal(charges.length);
   });
-  
+
   it('should have connectors that are valid types', () => {
-    const validConnectorTypes = ['CCS1', 'CCS2', 'CHAdeMO', 'Type 2', 'Tesla Supercharger', 'GBT', 'Tesla Wall Connector'];
+    const validConnectorTypes = ['CCS1', 'CCS2', 'CHAdeMO', 'Type 2',
+      'Tesla Supercharger', 'GBT', 'Tesla Wall Connector'];
     charges.forEach((charge) => {
       expect(charge).to.have.property('connectors');
       expect(charge.connectors).to.be.an('array');
@@ -92,37 +95,38 @@ describe('Vehicles JSON', () => {
       });
     });
   });
-  
+
   it('should have a valid power rating', () => {
     charges.forEach((charge) => {
       expect(charge).to.have.property('powerRating');
       expect(charge.powerRating).to.match(/^\d+(\.\d+)?\s+kW$/);
     });
   });
-  
+
   it('should have a valid modelname', () => {
     vehicles.forEach((vehicle) => {
       expect(vehicle).to.have.property('modelname');
       expect(vehicle.modelname).to.be.a('string');
     });
   });
-  
+
   it('should have valid connector type', () => {
     vehicles.forEach((vehicle) => {
       expect(vehicle).to.have.property('connectorType');
       expect(vehicle.connectorType).to.be.an('array');
       vehicle.connectorType.forEach((connector) => {
-        expect(['CCS1', 'CCS2', 'CHAdeMO', 'Type 2', 'Tesla Supercharger', 'GBT', 'Tesla Wall Connector']).to.include(connector);
+        expect(['CCS1', 'CCS2', 'CHAdeMO', 'Type 2',
+          'Tesla Supercharger', 'GBT', 'Tesla Wall Connector']).to.include(connector);
       });
     });
   });
-  
+
   it('should have a unique modelname for each vehicle', () => {
     const vehicleModels = vehicles.map((vehicle) => vehicle.modelname);
     const uniqueModels = new Set(vehicleModels);
     expect(uniqueModels.size).to.equal(vehicles.length);
   });
-  
+
   it('should have at least one connector type', () => {
     vehicles.forEach((vehicle) => {
       expect(vehicle).to.have.property('connectorType');
@@ -130,10 +134,10 @@ describe('Vehicles JSON', () => {
       expect(vehicle.connectorType.length).to.be.at.least(1);
     });
   });
-  
+
   it('should have a valid GBT connector type only for WM Motor EX5', () => {
     const wmMotorEx5 = vehicles.find((vehicle) => vehicle.modelname === 'WM Motor EX5');
     expect(wmMotorEx5).to.exist;
     expect(wmMotorEx5.connectorType).to.eql(['GBT']);
   });
-  });
+});
