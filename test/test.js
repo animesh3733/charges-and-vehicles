f0a290fe60aeac3a25ebb99854ae973794af4e4a
P PADMAPRASAD SHENOY <ppadmaprasadshenoy@gmail.com>
2023-04-08 22:14:06 +0530

    Changes

--- test/test.js
+++ test/test.js
@@ -7,6 +7,38 @@ const axios = require('axios');
 
 const baseUrl = 'https://raw.githubusercontent.com';
 
+describe('Vehicles JSON', () => {
+  beforeEach(() => {
+    nock(baseUrl)
+      .get('/ppadmaprasadshenoy/charges-and-vehicles/main/vehicles.json')
+      .reply(200, vehicles);
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('should return all vehicles', async () => {
+    const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/vehicles.json`);
+    expect(response.status).to.equal(200);
+    expect(response.data).to.deep.equal(vehicles);
+  });
+
+  it('should return a vehicle by modelname', async () => {
+    const modelname = 'Tesla Model 3';
+    const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/vehicles.json`);
+    const result = response.data.find(vehicle => vehicle.modelname === modelname);
+    expect(result).to.deep.equal(vehicles[0]);
+  });
+
+  it('should return empty array if no vehicles found with given connector type', async () => {
+    const connectorType = 'unknown';
+    const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/vehicles.json`);
+    const result = response.data.filter(vehicle => vehicle.connectorType.includes(connectorType));
+    expect(result).to.be.an('array').that.is.empty;
+  });
+});
+
 describe('Chargers JSON', () => {
   beforeEach(() => {
     nock(baseUrl)
@@ -29,6 +61,27 @@ describe('Chargers JSON', () => {
     const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/chargers.json`);
     const result = response.data.find(charge => charge.chargerModel === chargerModel);
     expect(result).to.deep.equal(charges[0]);
+  });
+
+  it('should return an empty array if no chargers are found', async () => {
+    const chargerModel = 'Non-existent Charger Model';
+    const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/chargers.json`);
+    const result = response.data.filter(charge => charge.chargerModel === chargerModel);
+    expect(result).to.be.an('array').that.is.empty;
+  });
+
+  it('should return chargers with the correct powerRating', async () => {
+    const powerRating = '360 kW';
+    const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/chargers.json`);
+    const result = response.data.every(charge => charge.powerRating === powerRating);
+    expect(result).to.be.false;
+  });
+
+  it('should return chargers with the correct connectors', async () => {
+    const connectors = ['Type 2'];
+    const response = await axios.get(`${baseUrl}/ppadmaprasadshenoy/charges-and-vehicles/main/chargers.json`);
+    const result = response.data.every(charge => charge.connectors.includes(...connectors));
+    expect(result).to.be.false;
   });
 });
 
